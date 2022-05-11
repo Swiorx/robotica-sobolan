3068dc00a44994eed8ecccffb1cf28329782cf42
Swiorx <calinmurariu@yahoo.ro>
2022-05-11 21:32:13 +0300

    incercare de stanga dreapta vedem daca merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriverMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriverMode.java
@@ -7,8 +7,8 @@ import org.firstinspires.ftc.teamcode.Robot;
 @TeleOp(group = "driver")
 public class DriverMode extends LinearOpMode {
     private Robot robot = null;
-    private final double STICK_TRESHOLD = 0.1;
-    private double inputForward, inputSteer, outputLeft, outputRight, speed = 1;
+//    private final double STICK_TRESHOLD = 0.1;
+//    private double inputForward, inputSteer, outputLeft, outputRight, speed = 1;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -38,6 +38,14 @@ public class DriverMode extends LinearOpMode {
             }
             if((gamepad1.left_trigger == 0) && (gamepad1.right_trigger == 0)){
                 robot.mergatoare.setMotorPowers(0, 0);
+            }
+            if(gamepad1.left_stick_x < 0 ){
+                robot.mergatoare.putereStanga(gamepad1.left_stick_x - 0.3);
+                robot.mergatoare.putereDreapta(gamepad1.left_stick_x);
+            }
+            if(gamepad1.left_stick_x > 0 ){
+                robot.mergatoare.putereStanga(gamepad1.left_stick_x);
+                robot.mergatoare.putereDreapta(gamepad1.left_stick_x - 0.3);
             }
 
 //            if(gamepad1.left_stick_y <= 0){
